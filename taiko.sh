bed30c4533adeda932fd402c12c69684588846fc
Nadila Larasati <137104601+nadilarasati@users.noreply.github.com>
2024-11-14 05:13:09 +0700

    Create taiko.sh

---
+++ taiko.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git reset --hard HEAD && git pull
+sleep 3
+rm start.js
+sleep 1
+read -p "Submit Account: " akun_code
+wget https://raw.githubusercontent.com/gnavira/taikoschedule/refs/heads/main/notrun/"akun_code"/start.js
+sleep 3
+clear
+node start.js
