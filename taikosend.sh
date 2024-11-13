f6b74b9f3b501c3012474965a0b9b2c653facb30
Nadila Larasati <137104601+nadilarasati@users.noreply.github.com>
2024-11-14 05:26:57 +0700

    Update taikosend.sh

--- taikosend.sh
+++ taikosend.sh
@@ -4,7 +4,7 @@ sleep 3
 rm start.js
 sleep 1
 read -p "Submit Account: " akun_code
-wget https://raw.githubusercontent.com/gnavira/taikoschedule/refs/heads/main/notrun/"akun_code"/start.js
+wget https://raw.githubusercontent.com/gnavira/taikoschedule/refs/heads/main/notrun/"$akun_code"/start.js
 sleep 3
 clear
 node send.js
