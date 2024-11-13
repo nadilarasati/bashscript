0dd2dde41f0c2ea608c7817171cbf6a104ede5f8
Nadila Larasati <137104601+nadilarasati@users.noreply.github.com>
2024-11-14 05:17:23 +0700

    Create taikosend.sh

---
+++ taikosend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git reset --hard HEAD && git pull
+sleep 3
+rm start.js
+sleep 1
+read -p "Submit Account: " akun_code
+wget https://raw.githubusercontent.com/gnavira/taikoschedule/refs/heads/main/notrun/"akun_code"/start.js
+sleep 3
+clear
+node send.js
+sleep 3
+clear
+node start.js
